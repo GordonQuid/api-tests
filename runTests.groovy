22cb019071d110a89e93781e6b70b7915768ad34
alexkobylnikov <akapoh@gmail.com>
2026-06-18 02:07:13 +0300

    fixes

--- runTests.groovy
+++ runTests.groovy
@@ -17,7 +17,7 @@ node("runner") {
             }
             stage("Allure report") {
                 allure(
-                        results: [[path: "/etc/jenkins/jenkins_home/workspace/root/jenkins/workspace/api-tests-run/allure-results"]],
+                        results: [[path: "allure-results"]],
                         disable: false,
                         reportBuildPolicy: "ALWAYS"
                 )
